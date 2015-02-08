eeb879065dcff256cdbbf9961ea823abe28c8d95
Ricci Adams <git@ricciadams.com>
2015-02-07 19:42:46 -0800

    Work on 1.1

--- Compatibility.m
+++ Compatibility.m
@@ -8,6 +8,178 @@
 
 #import "Compatibility.h"
 
-@implementation Compatibility
+#import <objc/runtime.h>
+#include <dlfcn.h>
 
-@end
+
+#define class_getInstanceMethod        _s0
+#define class_addMethod                _s1
+#define method_exchangeImplementations _s2
+#define class_getMethodImplementation  _s3
+#define method_getTypeEncoding         _s4
+#define NSSelectorFromString           _s5
+
+static Method (*class_getInstanceMethod)(Class cls, SEL name);
+static BOOL   (*class_addMethod)(Class cls, SEL name, IMP imp, const char *types);
+static void   (*method_exchangeImplementations)(Method m1, Method m2);
+SEL (*NSSelectorFromString)(NSString *aSelectorName);
+IMP (*class_getMethodImplementation)(Class cls, SEL name);
+const char *(*method_getTypeEncoding)(Method m);
+
+
+
+
+BOOL IsLegacyOS()
+{
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+
+    if ([processInfo respondsToSelector:@selector(operatingSystemVersion)]) {
+        NSOperatingSystemVersion version = [processInfo operatingSystemVersion];
+        
+        if (version.majorVersion > 10) {
+            return NO;
+        } else if (version.minorVersion >= 10) {
+            return NO;
+        }
+    }
+
+    return YES;
+}
+
+
+static inline void *sCompatibilityLookup(const UInt8 *inName)
+{
+    const UInt8 *i = inName;
+
+    UInt8 buffer[1024];
+    UInt8 *o = buffer;
+
+    while (*i) { *o = (*i - 128); i++; o++; }
+    *o = 0;
+
+    return dlsym(RTLD_NEXT, (char *)buffer);
+}
+
+
+static void sInit()
+{
+    if (class_getInstanceMethod) return;
+
+    UInt8 a[] = { 227,236,225,243,243,223,231,229,244,201,238,243,244,225,238,227,229,205,229,244,232,239,228,0 };
+    UInt8 b[] = { 227,236,225,243,243,223,225,228,228,205,229,244,232,239,228,0 };
+    UInt8 c[] = { 237,229,244,232,239,228,223,229,248,227,232,225,238,231,229,201,237,240,236,229,237,229,238,244,225,244,233,239,238,243,0 };
+    UInt8 d[] = { 206,211,211,229,236,229,227,244,239,242,198,242,239,237,211,244,242,233,238,231,0 };
+    UInt8 e[] = { 227,236,225,243,243,223,231,229,244,205,229,244,232,239,228,201,237,240,236,229,237,229,238,244,225,244,233,239,238,0 };
+    UInt8 f[] = { 237,229,244,232,239,228,223,231,229,244,212,249,240,229,197,238,227,239,228,233,238,231,0 };
+
+    class_getInstanceMethod = sCompatibilityLookup(a);
+    class_addMethod = sCompatibilityLookup(b);
+    method_exchangeImplementations = sCompatibilityLookup(c);
+    NSSelectorFromString = sCompatibilityLookup(d);
+    class_getMethodImplementation = sCompatibilityLookup(e);
+    method_getTypeEncoding = sCompatibilityLookup(f);
+}
+
+
+extern NSString *EmbraceCompatibilityLookup(UInt32 unused, ...)
+{
+    sInit();
+
+    va_list v;
+    va_start(v, unused);
+        
+    char *name = va_arg(v, char *);
+    
+    UInt8 *inName = (UInt8 *)name;
+    UInt8 *i      = inName;
+
+    UInt8 buffer[1024];
+    UInt8 *o = buffer;
+
+    while (*i) { *o = (*i - 128); i++; o++; }
+    *o = 0;
+
+    return [[NSString alloc] initWithBytes:buffer length:(o - buffer) encoding:NSASCIIStringEncoding];
+}
+
+
+void EmbraceCompatibilityLookup_(UInt32 unused, ...)
+{
+    sInit();
+
+    va_list v;
+    va_start(v, unused);
+
+    Class cls = NSClassFromString(va_arg(v, NSString *));
+    if (!cls) return;
+
+    SEL   originalSel = NSSelectorFromString(va_arg(v, NSString *));
+    SEL   altSel      = NSSelectorFromString(va_arg(v, NSString *));
+
+	Method originalMethod = class_getInstanceMethod(cls, originalSel);
+	Method altMethod      = class_getInstanceMethod(cls, altSel);
+
+    BOOL yn;
+
+    if (originalMethod && altMethod) {
+
+        yn = class_addMethod(cls,
+					originalSel,
+					class_getMethodImplementation(cls, originalSel),
+					method_getTypeEncoding(originalMethod));
+
+        yn = class_addMethod(cls,
+					altSel,
+					class_getMethodImplementation(cls, altSel),
+					method_getTypeEncoding(altMethod));
+
+        method_exchangeImplementations(class_getInstanceMethod(cls, originalSel), class_getInstanceMethod(cls, altSel));
+    }
+
+    va_end(v);
+}
+
+
+void EmbraceCheckCompatibility()
+{
+    if (IsLegacyOS()) return;
+
+    // Check for DebugUseDarkerTrackWindow
+    {
+        // Pref name = DebugUseDarkerTrackWindow
+        UInt8 n[] = { 196,229,226,245,231,213,243,229,196,225,242,235,229,242,212,242,225,227,235,215,233,238,228,239,247,0 };
+
+        // class = NSVisualEffectView
+        UInt8 c[] = { 206,211,214,233,243,245,225,236,197,230,230,229,227,244,214,233,229,247,0 };
+
+        // original selector = _internalMaterialFromMaterial:
+        UInt8 o[] = { 223,233,238,244,229,242,238,225,236,205,225,244,229,242,233,225,236,198,242,239,237,205,225,244,229,242,233,225,236,186,0 };
+
+        // alt selector = embrace_compatibility0:
+        UInt8 a[] = { 229,237,226,242,225,227,229,223,227,239,237,240,225,244,233,226,233,236,233,244,249,176,186,0 };
+    
+        if ([[NSUserDefaults standardUserDefaults] boolForKey:EmbraceGetPrivateName(n)]) {
+            EmbraceSizzle(EmbraceGetPrivateName(c), EmbraceGetPrivateName(o), EmbraceGetPrivateName(a));
+        }
+    }
+    
+    // Check for DebugForceTranslucency
+    {
+        // Pref name = DebugForceTranslucency
+        UInt8 n[] = { 196,229,226,245,231,198,239,242,227,229,212,242,225,238,243,236,245,227,229,238,227,249,0 };
+
+        // class = NSVisualEffectView
+        UInt8 c[] = { 206,211,214,233,243,245,225,236,197,230,230,229,227,244,214,233,229,247,0 };
+
+        // original selector = _useAccessibilityColors
+        UInt8 o[] = { 223,245,243,229,193,227,227,229,243,243,233,226,233,236,233,244,249,195,239,236,239,242,243,0 };
+
+        // alt selector = embrace_compatibility1
+        static UInt8 a[] = { 229,237,226,242,225,227,229,223,227,239,237,240,225,244,233,226,233,236,233,244,249,177,0 };
+
+        if ([[NSUserDefaults standardUserDefaults] boolForKey:EmbraceGetPrivateName(n)]) {
+            EmbraceSizzle(EmbraceGetPrivateName(c), EmbraceGetPrivateName(o), EmbraceGetPrivateName(a));
+        }
+    }
+}
+
--- Source/CurrentTrackController.m
+++ Source/CurrentTrackController.m
@@ -6,19 +6,80 @@
 //  Copyright (c) 2014 Ricci Adams. All rights reserved.
 //
 
-#import "LegacyCurrentTrackController.h"
+#import "CurrentTrackController.h"
 #import "Player.h"
 #import "EmbraceWindow.h"
 #import "WaveformView.h"
-#import "CloseButton.h"
 
-@interface LegacyCurrentTrackController () <PlayerListener, NSWindowDelegate>
+typedef NS_ENUM(NSInteger, CurrentTrackAppearance) {
+    CurrentTrackAppearanceWhite = 0,
+    CurrentTrackAppearanceLight = 1,
+    CurrentTrackAppearanceDark  = 2,
+};
+
+
+static CurrentTrackAppearance sGetCurrentTrackAppearance()
+{
+    return [[NSUserDefaults standardUserDefaults] integerForKey:@"CurrentTrackAppearance"];
+}
+
+
+static void sSetCurrentTrackAppearance(CurrentTrackAppearance appearance)
+{
+    [[NSUserDefaults standardUserDefaults] setInteger:appearance forKey:@"CurrentTrackAppearance"];
+}
+
+
+static BOOL sGetCurrentTrackPinning()
+{
+    return [[NSUserDefaults standardUserDefaults] boolForKey:@"CurrentTrackPinning"];
+}
+
+
+static void sSetCurrentTrackPinning(BOOL yn)
+{
+    [[NSUserDefaults standardUserDefaults] setBool:yn forKey:@"CurrentTrackPinning"];
+}
+
+
+
+@interface CurrentTrackController () <PlayerListener, NSWindowDelegate>
+@end
+
+@interface CurrentTrackControllerMainView : NSView
+@end
+
+@implementation CurrentTrackControllerMainView
+
+- (void) mouseDown:(NSEvent *)theEvent
+{
+    if ([theEvent type] == NSLeftMouseDown) {
+        NSEventModifierFlags modifierFlags = [NSEvent modifierFlags];
+        
+        if ((modifierFlags & (NSShiftKeyMask | NSControlKeyMask | NSAlternateKeyMask | NSCommandKeyMask)) == NSControlKeyMask) {
+            [NSMenu popUpContextMenu:[self menu] withEvent:theEvent forView:self];
+            return;
+        }
+    }
+    
+    [super mouseDown:theEvent];
+}
+
+
+- (void) rightMouseDown:(NSEvent *)theEvent
+{
+    if ([theEvent type] == NSRightMouseDown) {
+        [NSMenu popUpContextMenu:[self menu] withEvent:theEvent forView:self];
+    } else {
+        [super rightMouseDown:theEvent];
+    }
+}
+
 @end
 
 
-@implementation LegacyCurrentTrackController {
-    NSTrackingArea *_closeButtonTrackingArea;
-}
+
+@implementation CurrentTrackController
 
 - (NSString *) windowNibName
 {
@@ -29,20 +90,6 @@
 {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
     [[Player sharedInstance] removeObserver:self forKeyPath:@"currentTrack"];
-    
-    [[self mainView] removeTrackingArea:_closeButtonTrackingArea];
-}
-
-
-- (void) mouseEntered:(NSEvent *)theEvent
-{
-    [[(EmbraceWindow *)[self window] closeButton] setAlwaysVisible:YES];
-}
-
-
-- (void) mouseExited:(NSEvent *)theEvent
-{
-    [[(EmbraceWindow *)[self window] closeButton] setAlwaysVisible:NO];
 }
 
 
@@ -79,17 +126,131 @@
 }
 
 
-- (void) loadWindow
+- (void) _updateAppearance
 {
-    [super loadWindow];
+    CurrentTrackAppearance appearance = sGetCurrentTrackAppearance();
+    BOOL pinToBottom = sGetCurrentTrackPinning();
 
-    NSRect contentRect = [[self childWindow] frame];
-    NSUInteger styleMask = NSTitledWindowMask|NSClosableWindowMask|NSResizableWindowMask|NSTexturedBackgroundWindowMask;
+    NSWindow *window = [self window];
 
-    EmbraceWindow *window = [[EmbraceWindow alloc] initWithContentRect:contentRect styleMask:styleMask backing:NSBackingStoreBuffered defer:NO];
-    [window setupAsParentWindow];
+    if (appearance == CurrentTrackAppearanceWhite) {
+        [window setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameAqua]];
 
-    [self setWindow:window];
+        [[self effectView] setState:NSVisualEffectStateInactive];
+    
+        [[self leftLabel]  setTextColor:[NSColor blackColor]];
+        [[self rightLabel] setTextColor:[NSColor blackColor]];
+        [[self noTrackLabel] setTextColor:GetRGBColor(0x909090, 1.0)];
+    
+        [[self waveformView] setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameAqua]];
+        [[self waveformView] setActiveWaveformColor:  GetRGBColor(0x202020, 1.0)];
+        [[self waveformView] setInactiveWaveformColor:GetRGBColor(0xababab, 1.0)];
+
+    } else if (appearance == CurrentTrackAppearanceLight) {
+        [window setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantLight]];
+
+        [[self effectView] setState:NSVisualEffectStateActive];
+
+        [[self leftLabel]  setTextColor:[NSColor secondaryLabelColor]];
+        [[self rightLabel] setTextColor:[NSColor secondaryLabelColor]];
+        [[self noTrackLabel] setTextColor:[NSColor secondaryLabelColor]];
+
+        [[self waveformView] setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantLight]];
+        [[self waveformView] setActiveWaveformColor:  [NSColor secondaryLabelColor]];
+        [[self waveformView] setInactiveWaveformColor:[NSColor tertiaryLabelColor]];
+    
+    } else if (appearance == CurrentTrackAppearanceDark) {
+        [window setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
+
+        [[self effectView] setState:NSVisualEffectStateActive];
+
+        [[self leftLabel]  setTextColor:[NSColor secondaryLabelColor]];
+        [[self rightLabel] setTextColor:[NSColor secondaryLabelColor]];
+        [[self noTrackLabel] setTextColor:[NSColor secondaryLabelColor]];
+
+        [[self waveformView] setAppearance:[NSAppearance appearanceNamed:NSAppearanceNameVibrantDark]];
+        [[self waveformView] setActiveWaveformColor:  [NSColor colorWithCalibratedWhite:0.6  alpha:1.0]];
+        [[self waveformView] setInactiveWaveformColor:[NSColor colorWithCalibratedWhite:0.25 alpha:1.0]];
+    }
+    
+    [[window standardWindowButton:NSWindowCloseButton] setHidden:pinToBottom];
+
+    if (pinToBottom) {
+        [window setHasShadow:NO];
+        [window setStyleMask:([window styleMask] & ~NSResizableWindowMask)];
+        [window setMovable:NO];
+        [window setMovableByWindowBackground:NO];
+        [window setLevel:NSDockWindowLevel];
+
+        [window setCollectionBehavior:(
+            NSWindowCollectionBehaviorCanJoinAllSpaces |
+            NSWindowCollectionBehaviorTransient |
+            NSWindowCollectionBehaviorIgnoresCycle |
+            NSWindowCollectionBehaviorFullScreenAuxiliary
+        )];
+
+    } else {
+        [window setHasShadow:YES];
+        [window setStyleMask:([window styleMask] | NSResizableWindowMask)];
+        [window setMovable:YES];
+        [window setMovableByWindowBackground:YES];
+        [window setLevel:NSNormalWindowLevel];
+
+        [window setCollectionBehavior:NSWindowCollectionBehaviorDefault];
+    }
+
+    [[self waveformView] redisplay];
+}
+
+
+- (void) _updateWindowFrame
+{
+    if (!sGetCurrentTrackPinning()) return;
+
+    NSRect oldFrame = [[self window] frame];
+    
+    NSScreen *screen = [NSScreen mainScreen];
+    
+    NSRect visibleFrame = [screen visibleFrame];
+    NSRect frame        = [screen frame];
+
+    // Dock on left
+    if (visibleFrame.origin.x > frame.origin.x) {
+        if (visibleFrame.origin.x < 10) {
+            visibleFrame.size.width += visibleFrame.origin.x;
+            visibleFrame.origin.x = 0;
+
+        } else {
+            visibleFrame.origin.x   -= 1.0;
+            visibleFrame.size.width += 1.0;
+        }
+
+    // Dock on right
+    } else if (visibleFrame.size.width < frame.size.width) {
+        CGFloat dockWidth = frame.size.width - visibleFrame.size.width;
+        
+        if (dockWidth < 10) {
+            visibleFrame.size.width += dockWidth;
+        }
+        
+    // Dock on bottom
+    } else {
+        if (visibleFrame.origin.y  < 10) {
+            visibleFrame.origin.y = 0;
+        }
+    }
+
+    NSRect newFrame = visibleFrame;
+    
+    newFrame.size.height = oldFrame.size.height;
+    
+    [[self window] setFrame:newFrame display:NO];
+}
+
+
+- (void) windowDidResize:(NSNotification *)notification
+{
+    [self _updateWindowFrame];
 }
 
 
@@ -97,13 +258,24 @@
 {
     [super windowDidLoad];
 
-    NSWindow *parentWindow = [self window];
-    NSWindow *childWindow  = [self childWindow];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_handleNSApplicationDidChangeScreenParameters:) name:NSApplicationDidChangeScreenParametersNotification object:nil];
 
-    [parentWindow setDelegate:self];
-    [parentWindow setFrameAutosaveName:@"CurrentTrackWindow"];
+    NSWindow *window = [self window];
 
-    if (![parentWindow setFrameUsingName:@"CurrentTrackWindow"]) {
+    [window setDelegate:self];
+    [window setFrameAutosaveName:@"CurrentTrackWindow"];
+
+    [window setTitleVisibility:NSWindowTitleHidden];
+    [window setTitlebarAppearsTransparent:YES];
+
+    [[window standardWindowButton:NSWindowMiniaturizeButton] setHidden:YES];
+    [[window standardWindowButton:NSWindowZoomButton] setHidden:YES];
+
+    [[self mainView] setFrame:[[self effectView] bounds]];
+    [[self mainView] setAutoresizingMask:NSViewHeightSizable|NSViewWidthSizable];
+    [[self effectView] addSubview:[self mainView]];
+    
+    if (![window setFrameUsingName:@"CurrentTrackWindow"]) {
         NSScreen *screen = [[NSScreen screens] firstObject];
         
         NSRect screenFrame = [screen visibleFrame];
@@ -114,13 +286,9 @@
         windowFrame.origin.x = round((screenFrame.size.width - windowFrame.size.width) / 2);
         windowFrame.origin.x += screenFrame.origin.x;
     
-        [[self window] setFrame:windowFrame display:NO];
+        [window setFrame:windowFrame display:NO];
     }
     
-    [childWindow setIgnoresMouseEvents:YES];
-    [childWindow setBackgroundColor:[NSColor clearColor]];
-    [childWindow setOpaque:NO];
-
     Player *player = [Player sharedInstance];
     [self setPlayer:[Player sharedInstance]];
 
@@ -128,44 +296,65 @@
 
     [self _updateTrack];
     
-    [[self mainView] setPostsBoundsChangedNotifications:YES];
-    
     [[Player sharedInstance] addListener:self];
 
     [self _updateTrack];
 
-    [parentWindow setMinSize:[childWindow minSize]];
-
-    NSTrackingAreaOptions options = NSTrackingActiveAlways | NSTrackingMouseEnteredAndExited | NSTrackingInVisibleRect;
-    _closeButtonTrackingArea = [[NSTrackingArea alloc] initWithRect:NSZeroRect options:options owner:self userInfo:nil];
-
-    [[self mainView] addTrackingArea:_closeButtonTrackingArea];
-
     [[self window] setExcludedFromWindowsMenu:YES];
-}
-
-
-- (void) windowWillClose:(NSNotification *)notification;
-{
-    if ([notification object] == [self childWindow]) {
-        [[self window] orderOut:self];
-    } else {
-        [[self childWindow] orderOut:self];
-    }
-}
-
-
-- (IBAction) showWindow:(id)sender
-{
-    NSDisableScreenUpdates();
-
-    [[self window] addChildWindow:[self childWindow] ordered:NSWindowAbove];
-    [[self window] setFrame:[[self window] frame] display:YES];
-
-    [super showWindow:sender];
-    [[self childWindow] orderFront:self];
     
-    NSEnableScreenUpdates();
+    [self _updateAppearance];
+}
+
+
+- (BOOL) validateMenuItem:(NSMenuItem *)menuItem
+{
+    NSString *actionString = NSStringFromSelector([menuItem action]);
+
+    if ([actionString isEqualToString:@"changeAppearance:"]) {
+        NSInteger tag = [menuItem tag];
+        
+        if (sGetCurrentTrackAppearance() == tag) {
+            [menuItem setState:NSOnState];
+        } else {
+            [menuItem setState:NSOffState];
+        }
+    
+        return YES;
+
+    } else if ([actionString isEqualToString:@"changePinning:"]) {
+        [menuItem setState:sGetCurrentTrackPinning() ? NSOnState : NSOffState];
+        return YES;
+    }
+    
+    return NO;
+}
+
+
+- (void) showWindow:(id)sender
+{
+    [self _updateWindowFrame];
+    [super showWindow:sender];
+}
+
+
+- (IBAction) changeAppearance:(id)sender
+{
+    sSetCurrentTrackAppearance([sender tag]);
+    [self _updateAppearance];
+}
+
+
+- (IBAction) changePinning:(id)sender
+{
+    sSetCurrentTrackPinning([sender state] == NSOffState);
+    [self _updateAppearance];
+    [self _updateWindowFrame];
+}
+
+
+- (void) _handleNSApplicationDidChangeScreenParameters:(NSNotification *)note
+{
+    [self _updateWindowFrame];
 }
 
 
