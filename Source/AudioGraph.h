cd7f3935b84997d74a7d907962e165bd63f6bb70
Ricci Adams <git@ricciadams.com>
2018-11-22 03:53:32 -0800

    Stub

---
+++ Source/AudioGraph.h
@@ -0,0 +1,17 @@
+//
+//  AudioGraph.h
+//  Embrace
+//
+//  Created by Ricci Adams on 2018-11-21.
+//  Copyright © 2018 Ricci Adams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AudioGraph : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
