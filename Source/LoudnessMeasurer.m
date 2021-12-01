fe104b56d311e757395207a385ac92ba13e84bfe
Ricci Adams <git@ricciadams.com>
2021-11-30 21:47:14 -0800

    Only turn on FTZ bit on Intel

--- Source/LoudnessMeasurer.m
+++ Source/LoudnessMeasurer.m
@@ -30,7 +30,10 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#if defined(__x86_64__)
 #include <xmmintrin.h>
+#endif
 
 #include <Accelerate/Accelerate.h>
 #include <AudioToolbox/AudioToolbox.h>
@@ -214,8 +217,10 @@ static inline void sFilter(const LoudnessMeasurer *self, LoudnessMeasurerChannel
         channel->_samplePeak = max;
     }
 
+#if defined(__x86_64__)
     unsigned int mxcsr = _mm_getcsr();
     _mm_setcsr(mxcsr | _MM_FLUSH_ZERO_ON);
+#endif
 
     double *s1 = channel->_scratch  + 2;
     double *s2 = channel->_scratch2 + 2;
@@ -237,7 +242,9 @@ static inline void sFilter(const LoudnessMeasurer *self, LoudnessMeasurerChannel
         bufferPost[i] = s1[i];
     }
 
+#if defined(__x86_64__)
     _mm_setcsr(mxcsr);
+#endif
 }
 
 
